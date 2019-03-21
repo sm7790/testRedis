31fccaee4470cd3400e2f4327697d36a009b04e8
Alberto Jimenez <alberto.jimenez@grupocmc.es>
2019-03-21 12:23:53 +0100

    try travis

--- src/main/java/com/gruposantander/subscribersarq/SubscriberSarqApplication.java
+++ src/main/java/com/gruposantander/subscribersarq/SubscriberSarqApplication.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 @SpringBootApplication
 @EnableCaching
 public class SubscriberSarqApplication {
-
+		// fgdfgfdgfdgfdgfdgfdgfdgfdgdfd
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriberSarqApplication.class, args);
 	}
